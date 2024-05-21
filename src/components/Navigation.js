fdff50ae87e6d0b79247c91288e59367075d33ec
kamihadadg <83748600+kamihadadg@users.noreply.github.com>
2024-05-21 17:34:53 +0330

    C

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -35,9 +35,9 @@ const Navigation = () => {
           </Typography>
           <Box>
             <Button color="inherit" component={Link} to="/">{t('home')}</Button>
-            <Button color="inherit" component={Link} to="/Educate">{t('Educate')}</Button>
+            <Button color="inherit" component={Link} to="/educate">{t('Educate')}</Button>
             <Button color="inherit" component={Link} to="/projects">{t('projects')}</Button>
-            <Button color="inherit" component={Link} to="/Customers">{t('Customers')}</Button>
+            <Button color="inherit" component={Link} to="/customers">{t('Customers')}</Button>
             {/* <Button color="inherit" component={Link} to="/about">{t('about')}</Button> */}
             <Button color="inherit" component={Link} to="/contact">{t('contact')}</Button>
             <Select 
